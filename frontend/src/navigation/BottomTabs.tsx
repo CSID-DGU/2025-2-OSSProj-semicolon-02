b75a60d64ea7a4ae8256fccc31104d771d4843f1
asle149 <gwsak10@gmail.com>
2025-11-04 18:06:32 +0900

    fix: 마이페이지 수정

--- frontend/src/navigation/BottomTabs.tsx
+++ frontend/src/navigation/BottomTabs.tsx
@@ -7,7 +7,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from '../screens/HomeScreen';
 import AddTabButton from '../components/AddTabButton';
-import MyPageScreen from '../screens/MyPageScreen';
+import MyPageScreen from '../screens/MyPage/MyPageScreen';
 import FabMenu from '../components/FabMenu';
 import TabBarBackground from '../components/TabBarBackground';
 import type { TabParamList } from './types';
